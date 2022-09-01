0ef8ea8bb8551a1d82e7e777542bf5ac7ef72f79
singleton5 <onyeani.james@gmail.com>
2022-09-01 08:20:38 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,34 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+TRUNCATE_TABLES=$($PSQL "TRUNCATE TABLE games, teams")
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+if [[ $WINNER != "winner" || $OPPONENT != "opponent" ]] 
+then
+#to check if team has already been inserted
+TEAM_ID_W=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+TEAM_ID_O=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+#if team from winners section is not found
+  if [[ -z $TEAM_ID_W ]]
+  then
+  INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+  
+  #get new team_id_w
+  TEAM_ID_W=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+  fi
+
+#if team from opponent section is not found
+  if [[ -z $TEAM_ID_O ]]
+  then
+  INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+  
+  #get new team_id_o
+  TEAM_ID_O=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+  fi
+  
+  INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $TEAM_ID_W, $TEAM_ID_O, $WINNER_GOALS, $OPPONENT_GOALS)")
+
+fi
+done
